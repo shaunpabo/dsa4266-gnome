4370f94159c27d009632d47621b523c604d80a47
anselcheung <anselcheung@yahoo.com.sg>
2023-11-09 11:07:13 +0800

    Change num cores for predict

--- predict_main.py
+++ predict_main.py
@@ -59,7 +59,7 @@ if __name__=='__main__':
         context = get_context('fork')
         print(f'Starting predict task with {num_cores} cpu cores')
         # create the process pool
-        with ProcessPoolExecutor(max_workers=num_cores, mp_context=context) as executor:
+        with ProcessPoolExecutor(max_workers=2, mp_context=context) as executor:
             # perform calculations
             df_list = []
             futures = [executor.submit(task, folder) for folder in all_folders]
