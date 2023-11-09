c3baa29bf3f39bf5d49b2feaa869fd054d990c55
anselcheung <anselcheung@yahoo.com.sg>
2023-11-09 09:38:48 +0800

    Use requirements.txt for setup

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,5 +1,5 @@
 sudo apt install python3-pip -y
-pip3 install pandas, scikit-learn, category-encoders, xgboost
+pip3 install -r requirements.txt
 mkdir m6anet
 cd m6anet
 aws s3 sync --no-sign-request s3://sg-nex-data/data/processed_data/m6Anet/ .
